9059216ddbc793ae756c0e1e13c887847af82ecd
MathiasGR27 <132688522+MathiasGR27@users.noreply.github.com>
2023-11-22 10:58:23 -0500

    Practica 4

---
+++ ListaGualpaMathias/src/ListasGualpaMathias/Lista.java
@@ -0,0 +1,21 @@
+package ListasGualpaMathias;
+
+import java.util.ArrayList;
+
+public class Lista {
+	
+	public static void main(String[] args) {
+	
+		ArrayList<String> listaColores = new ArrayList<>();
+		listaColores.add("Rojo");
+		listaColores.add("Amarrillo");
+		
+		System.out.println("Lista de colores :");
+		for ( String color :listaColores) {
+			System.out.println(listaColores);
+		}
+	
+		
+
+    }
+}
