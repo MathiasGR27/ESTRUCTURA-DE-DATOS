7428341f87fcb94c10618ec22b4c39dd742ce43e
MathiasGR27 <132688522+MathiasGR27@users.noreply.github.com>
2024-01-07 18:09:31 -0500

    U2Practica3_GualpaMathias

---
+++ U2Practica3_Ordenar/src/u2practica3_ordenar/OrdenacionShellApp.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package u2practica3_ordenar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gisse
+ */
+public class OrdenacionShellApp {
+
+	public static void inicio() {
+		/*
+		 Dado un numero entero no negativo, retornarlo con sus digitos en orden descendente
+		 */
+		
+		ArregloShell arr;
+		
+		// Ingresamos numero a analizar
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Ingrese numero entero: ");
+		int numero=scanner.nextInt();
+		
+		// contamos el numero de digitos
+		int n=numero;
+		int numDigitos=0;
+		while (n>0) {
+			n=n/10;
+			numDigitos++;
+		}
+		
+		// obtenemos cada digito del numero y lo ingresamos al arreglo
+		long cociente;
+		long digito;
+		long temp=numero;
+		arr=new ArregloShell(numDigitos);
+		while(temp!=0) {
+			cociente=temp/10;
+			digito=temp-cociente*10;
+			arr.insertar(digito);
+			temp=cociente;
+		}
+		
+		// ordenamos el arreglo usando la ordenacion Shell
+		arr.ordenacionShell();
+		
+		// imprimimos valores de arreglo desde ultimo elemento hasta el primero
+		arr.mostrarElementosOrdenInverso();
+		
+	}
+}
---
+++ U2Practica3_Ordenar/src/u2practica3_ordenar/OrdenarInsercion.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package u2practica3_ordenar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gisse
+ */
+public class OrdenarInsercion {
+    public static void inicio(){
+        
+    // creamos instancia de la clase ArregloInsercion con una capacidad para almacenar 10 elementos
+		ArregloInsercion arr=new ArregloInsercion(10);
+		
+		// insertamos 6 valores al arreglo
+		arr.insertar(16);
+		arr.insertar(11);
+		arr.insertar(56);
+		arr.insertar(4);
+		arr.insertar(87);
+		arr.insertar(78);
+		
+		// solicitamos valor para el cual queremos averiguar los numeros en el arreglo menores a el
+		Scanner entrada=new Scanner(System.in);
+		System.out.print("Ingrese valor: ");
+		int valor=entrada.nextInt();
+		
+		// mostramos elementos insertados
+		System.out.print("Elementos insertados: ");
+		arr.mostrarElementos();
+		
+		// imprimimos valores menores al valor de la variable valor
+		System.out.print("Elementos menores a " + valor + ": ");
+		arr.mostrarElementosMenoresA(valor);
+	}
+}
